0c56a52c2af4dff83ad5b0921fe1c1001329929f
minak777 <minasek005@gmail.com>
2024-03-15 21:59:21 +0300

    modified:   lib/favpages/favpage.dart 	deleted:    lib/ProfilePage.dart 	deleted:    "lib/assets/logo/Zeweter\341\213\230\341\213\210\341\211\265\341\210\255 (3).png" 	modified:   lib/homepage/hi_notify.dart 	modified:   lib/landing.dart 	modified:   lib/logo.dart 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   test/widget_test.dart

--- lib/favpages/favpage.dart
+++ lib/favpages/favpage.dart
@@ -16,53 +16,59 @@ class _FavPageState extends State<FavPage> {
     'Item 4',
     'Item 5',
     'Item 6',
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6',
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          padding: EdgeInsets.all(8.0),
-          child: Text(
-            'Favorites',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
+    return Padding(
+      padding: const EdgeInsets.only(top: 60),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              'Favorites',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                     
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
                   child: Material(
-                    elevation: 5,
+                    elevation: 0.5,
+                    borderRadius: BorderRadius.circular(10),
                     child: ListTile(
                       leading: CircleAvatar(
                         backgroundImage: AssetImage('lib/assets/mic.jpg'),
                       ),
+                      trailing: ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Unfollow'),
+                      ),
                       title: Text('Sportpod'),
-                      onTap: () {
-                        // Add your onTap logic here
-                        print('Tapped on item ${items[index]}');
-                      },
+                      onTap: () {},
                     ),
                   ),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
